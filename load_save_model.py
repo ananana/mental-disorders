3062ebfc852da817c08eb336dd69d6895921f7e4
Ana Sabina Uban <ana.uban@gmail.com>
2021-04-02 21:22:37 +0200

    added model saving/loading

--- load_save_model.py
+++ load_save_model.py
@@ -1,7 +1,7 @@
+from metrics import Metrics
+import json
 def save_model_and_params(model, model_path, hyperparams, hyperparams_features):
     model.save_weights(model_path + "_weights.h5", save_format='h5')
-#     model.save(model_path + "_model.model")
-#     model.save(model_path + "_model.h5")
     with open(model_path + '.hp.json', 'w+') as hpf:
         hpf.write(json.dumps({k:v for (k,v) in hyperparams.items() if k!='optimizer'}))
     with open(model_path + '.hpf.json', 'w+') as hpff:
@@ -25,10 +25,9 @@ def load_saved_model(model_path, hyperparams):
     'BertLayer': BertLayer
     }
     loaded_model = load_model(model_path + "_model.h5", custom_objects=dependencies)
-#     loaded_model = load_model(model_path + "_model.model", custom_objects=dependencies)
     return loaded_model
 
-def load_saved_model_weights(model_path, hyperparams, emotions, stopword_list, liwc_categories, classes, h5=False):
+def load_saved_model_weights(model_path, hyperparams, stopwords_dim, h5=False):
     metrics_class = Metrics(threshold=hyperparams['threshold'])
     dependencies = {
     'f1_m': metrics_class.f1_m,
@@ -38,17 +37,12 @@ def load_saved_model_weights(model_path, hyperparams, emotions, stopword_list, l
     'binary_crossentropy_custom': binary_crossentropy_custom,
     'BertLayer': BertLayer
     }
-    loaded_model = initialize_model(hyperparams=hyperparams, hyperparams_features=hyperparams_features, 
-                                    embedding_matrix=embedding_matrix, 
-                                 emotions=emotions, stopword_list=stopword_list, liwc_categories=liwc_categories,
-                                classes=classes)
+    loaded_model = initialize_model(hyperparams, hyperparams_features, stopwords_dim)
     loaded_model.summary()
-#     loaded_model.load_weights(model_path + "_weights")
     path = model_path + "_weights"
     by_name = False
     if h5:
         path += ".h5"
         by_name=True
     loaded_model.load_weights(path, by_name=by_name)
-    #     loaded_model = load_model(model_path + "_model.model", custom_objects=dependencies)
     return loaded_model
