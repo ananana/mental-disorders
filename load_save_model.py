2ea8cd736fe6f056e617d7448e4996ebc9005b03
Ana Sabina Uban <ana.uban@gmail.com>
2021-04-03 13:46:12 +0200

    moving around config paths and params

--- load_save_model.py
+++ load_save_model.py
@@ -10,11 +10,16 @@ def save_model_and_params(model, model_path, hyperparams, hyperparams_features):
     with open(model_path + '.hpf.json', 'w+') as hpff:
         hpff.write(json.dumps(hyperparams_features))
 
-def load_params(model_path):
+def load_params(model_path, general_config_path='config.json'):
     with open(model_path + '.hp.json', 'r') as hpf:
         hyperparams = json.loads(hpf.read())
     with open(model_path + '.hpf.json', 'r') as hpff:
         hyperparams_features = json.loads(hpff.read())
+    with open(general_config_path) as f:
+        config = json.load(f)
+    for k in config:
+        if k not in hyperparams_features:
+            hyperparams_features[k] = config = k
     hyperparams['optimizer'] = optimizers.Adam(lr=hyperparams['lr'], #beta_1=0.9, beta_2=0.999, epsilon=0.0001,
                                    decay=hyperparams['decay'])
     return hyperparams, hyperparams_features
@@ -29,14 +34,14 @@ def load_saved_model(model_path, hyperparams):
     loaded_model = load_model(model_path + "_model.h5", custom_objects=dependencies)
     return loaded_model
 
-def load_saved_model_weights(model_path, hyperparams, hyperparams_features, stopwords_dim, h5=False):
+def load_saved_model_weights(model_path, hyperparams, hyperparams_features, h5=False):
     metrics_class = Metrics(threshold=hyperparams['threshold'])
     dependencies = {
     'f1_m': metrics_class.f1_m,
     'precision_m': metrics_class.precision_m,
     'recall_m': metrics_class.recall_m,
     }
-    loaded_model = initialize_model(hyperparams, hyperparams_features, stopwords_dim)
+    loaded_model = initialize_model(hyperparams, hyperparams_features)
     loaded_model.summary()
     path = model_path + "_weights"
     by_name = False
