96bb7f60ca0991f0cf7cfa80a5634d6454f3becf
Ana Sabina Uban <ana.uban@gmail.com>
2021-04-04 16:49:27 +0200

    improved erisk style alerts, now by users, including rolling average, conservative, etc

--- predict_erisk.py
+++ predict_erisk.py
@@ -12,6 +12,38 @@ RUNS_MODEL_PATHS = {
                                                       # depression+anorexia, trained on eRisk self-harm
     }
 
+def scores_to_alerts(predictions_dict, posts_per_datapoint=None, conservative_alerts=True, 
+                     alert_threshold=0.5, rolling_window=0):
+    '''Generates alerts decisions (1/0) from a dictionary of prediction scores per user
+    Parameters:
+        predictions_dict: dictionary with ordered predictions per user (indexed by user id) 
+        rolling_window: window of rolling average to be computed across prediction scores
+            history for a given user in order to get a "smoothed" prediction for each datapoint
+            If 0, then no rolling average is computed.
+        conservative_alerts: if True, will only emit positive alerts if enough input posts are
+            used for prediction (will only trust predictions based on at least as many posts
+            as were used in one datapoint in the training stage)
+        posts_per_datapoint: integer denoting number of posts per datapoint used in the training stage
+            used in case of conservative_alerts=True
+        alert_threshold: threshold on the score value above which to emit a positive alert
+        Returns: nested dictionary indexed by users, including the original prediction score
+            ('scores' key) and the alert value (1/0) (the 'alerts' key)'''
+    users = predictions_dict.keys()
+    scores_per_user = dict(predictions_dict)
+    def _rolling_average(scores, window):
+        if window < len(scores):
+            return scores
+        rolling_predictions[:rolling_window-1] = predictions[:rolling_window-1]
+        rolling_predictions.extend(np.convolve(predictions, np.ones(rolling_window), 'valid') / rolling_window)
+        return rolling_predictions
+    if rolling_window:
+        scores_per_user = {u: _rolling_average(scores_per_user[u], rolling_window) for u in users}
+    alerts_per_user = {}
+    # TODO: implement conservative alerts here?
+    for u in users:
+        alerts_per_user[u] = [int(p>=alert_threshold) for p in scores_per_user[u]]
+    return {u: {'scores': scores_per_user[u], 'alerts': alerts_per_user[u]} for u in users}
+
 def predict(run_nr, data_rounds, alert_threshold=0.5, rolling_window=50, conservative_alerts=True):
     """
     Expects a run_nr corresponding to the solution to be used for generating predictions.
@@ -19,14 +51,17 @@ def predict(run_nr, data_rounds, alert_threshold=0.5, rolling_window=50, conserv
     implementation and performance are found there.
     Every solution uses a different trained model, or a different strategy for generating
     predictions.
-    
+    Will generate one for prediction every x posts for each user, where x is the number of posts 
+    in one chunk/datapoint used in training for this model
+    If number of posts available < x, it will still generate a prediction, alerts will be 0
+    by default (in the conservative setting, controlled by a flag)
     Parameters:
     run_nr: integer representing the solution/run to be used
     data_rounds: a list of dictionaries containing data corresponding to one post / one round
                 in the stream, in the format used by the eRisk server
     
     Returns:
-    a list of predictions (0/1)
+    a dictionary of scores and alerts (1/0) per user in the input data
     """
     
     model_path = RUNS_MODEL_PATHS[run_nr]
@@ -46,24 +81,17 @@ def predict(run_nr, data_rounds, alert_threshold=0.5, rolling_window=50, conserv
     for data_round in data_rounds:
         data_generator.add_data_round(data_round)
 
-    predictions = model.predict(data_generator)
 
-    # TODO: implement rolling average per user
-    # Use rolling average on prediction scores
-    if run_nr==3:
-        rolling_predictions = []
-        # The first predictions will be copied
-        rolling_predictions[:rolling_window-1] = predictions[:rolling_window-1]
-        # rolling average over predictions
-        rolling_predictions.extend(np.convolve(predictions, np.ones(rolling_window), 'valid') / rolling_window)
-        # predictions = rolling_predictions
-
-    if conservative_alerts and len(data_rounds) < hyperparams['posts_per_group']:
-        alerts = [0 for p in predictions]
-    else:
-        alerts = [int(p >= 0.5) for p in predictions]
-
-    return alerts
+    predictions_per_user = {}
+    for dp in data_generator:
+        prediction = model.predict_step(dp)
+        u = dp[1][0]
+        if u not in predictions_per_user:
+            predictions_per_user[u] = []
+        predictions_per_user[u].append(prediction.numpy().item())
+    alerts_per_user = scores_to_alerts(predictions_per_user, rolling_window=rolling_window,
+        conservative_alerts=(conservative_alerts and len(data_rounds) < hyperparams['posts_per_group']))
+    return alerts_per_user
 
 if __name__=='__main__':
         # Reading eRisk data
@@ -94,5 +122,6 @@ if __name__=='__main__':
     "date": "2017-05-09T17:02:50.000+0000", 
     "id": 169532, 
     "title": "    Nioh - Become a visitor", "number": 2, "nick": "subject992"}]
+
 
     print(predict(run_nr=1, data_rounds=[data_round1, data_round2]))
